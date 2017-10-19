7b9cec69ebe89a251d66e18ed360c5f789599cb5
svandex <svandex@users.noreply.github.com>
2017-10-19 16:09:15 +0800

    test file for TDMS

--- code/ionproc.cpp
+++ code/ionproc.cpp
@@ -1,8 +1,34 @@
 #include<iostream>
 #include<nilibddc.h>
-#include<qcustomplot.h>
 
 int main(int argc, char* argv[]){
-		
+	if(argc!=2){
+		std::cout<<"ARGUMENT NOT ENOUGH!"<<std::endl;
+		return 1;
+	}
+	int ddcError=0;
+	DDCFileHandle file=0;
+
+//	std::cout<<argv[1]<<std::endl;
+
+	//open file
+	//ddcError=DDC_OpenFileEx(argv[1],DDC_FILE_TYPE_TDM_STREAMING,1,&file);
+	ddcError=DDC_OpenFileEx(argv[1],"TDMS",1,&file);
+	if(ddcError==0){
+		unsigned int ncg;
+		ddcError=DDC_GetNumChannelGroups(file,&ncg);
+
+		unsigned int numberOfProperties;
+		DDC_GetNumFileProperties(file,&numberOfProperties);
+		if(ddcError==0){
+			std::cout<<"Number of Channal Groups: "<<ncg<<std::endl;
+			std::cout<<"Number of File Properties: "<<numberOfProperties<<std::endl;
+		}else{
+			std::cout<<"ddcError: GetNum Error, "<<ddcError<<" ,"<<DDC_GetLibraryErrorDescription(ddcError)<<std::endl;
+		}	
+	}else{
+		std::cout<<"ddcError: OpenFile Error, "<<ddcError<<" ,"<<DDC_GetLibraryErrorDescription(ddcError)<<std::endl;
+	}
+	DDC_CloseFile(file);
 	return 0;
 }
