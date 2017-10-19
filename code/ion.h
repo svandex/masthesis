830076ea9c985818fc43378da3a9b138c7a824bb
svandex <svandex@users.noreply.github.com>
2017-10-19 20:15:50 +0800

    modification

--- code/ion.h
+++ code/ion.h
@@ -1,17 +1,23 @@
 #include<iostream>
 #include<nilibddc.h>
+#include<vector>
 
-struct{
+struct tdmsAttr{
 	unsigned int numberOfProperties;
 	char **propertyNames;//Property Names in TDMS file 
-}tdmsAttr;
+	std::vector<DDCDataType> propertyDataType;
+};
 
-class ion{//Initial Processing of TDMS file and PE file, PE for photoelectric encoder.
+//Initial Processing of TDMS file and PE file, PE for photoelectric encoder.
+class ion{
 public:
-	ion(const char* f, const char* t):pepath(f),tdmspath(t){ddcError=0;};
+	ion(const char* p, const char* t);
 	ion(const ion&i)=delete;
+	~ion();
 protected:
 	DDCFileHandle file;
+	unsigned int numOfCycle;
+	tdmsAttr ta;
 private:
 	const char* pepath;
 	const char* tdmspath;
@@ -20,9 +26,9 @@ private:
 
 std::ostream& operator<< (std::ostream& os, const ion& i);//Output all properties in format
 
-struct{
+struct ionAttr{
 	
-}ionAttr;
+};
 
 //IO reading from TDMS file and writing cycle data to a file
 class ioncyc:public ion{
