742b7524037c481fc8722168e1f9f25f674fedca
svandex <svandex@users.noreply.github.com>
2017-10-27 17:51:23 +0800

    Show Property of Each channel

--- code/ion.cpp
+++ code/ion.cpp
@@ -21,23 +21,31 @@ ion::ion(const char* p, const char* t):pepath(p),tdmspath(t){
 	//Check which channel is ion	
 #ifdef DEBUG
 	errorChk(DDC_GetNumChannelGroups(file,&numOfChannelGroups));
-	DDCChannelGroupHandle* cghtemp=(DDCChannelGroupHandle*)malloc(sizeof(DDCChannelGroupHandle)*(numOfChannelGroups+1));
-	errorChk(DDC_GetChannelGroups(file,&cghtemp,numOfChannelGroups));	
+	DDCChannelGroupHandle* cghtemp=(DDCChannelGroupHandle*)calloc(numOfChannelGroups,sizeof(DDCChannelGroupHandle));
+	errorChk(DDC_GetChannelGroups(file,cghtemp,numOfChannelGroups));	
 
 	for(size_t i=1;i<=numOfChannelGroups;i++){
 		std::cout<<"Channel Groups : "<<i<<std::endl;
 		unsigned int numOfChannels;
 		errorChk(DDC_GetNumChannels(cghtemp[i-1],&numOfChannels));
-		DDCChannelHandle* chtemp = (DDCChannelHandle*)malloc(sizeof(DDCChannelHandle)*(numOfChannels+1)); 	
-		errorChk(DDC_GetChannels(cghtemp[i-1],&chtemp,numOfChannels));
+		DDCChannelHandle* chtemp = (DDCChannelHandle* )calloc(numOfChannels,sizeof(DDCChannelHandle));
+		errorChk(DDC_GetChannels(cghtemp[i-1],chtemp,numOfChannels));
 
 		for(size_t j=1;j<=numOfChannels;j++){
+			std::cout<<"	Channel : "<<j<<std::endl;
 			unsigned int numOfProperties;
-			errorChk(DDC_GetNumChannelProperties(chtemp,&numOfProperties));
-			std::cout<<"		"<<j<<std::endl;
+			errorChk(DDC_GetNumChannelProperties(chtemp[j-1],&numOfProperties));
+			char* property=0;
+			unsigned int length;
+
+			errorChk(DDC_GetChannelStringPropertyLength(chtemp[j-1],DDC_CHANNEL_NAME,&length));
+
+			property = (char*)malloc(length+1);
+			DDC_GetChannelProperty(chtemp[j-1],DDC_CHANNEL_NAME,property,length+1);
+			std::cout<<"		Property: "<<property<<std::endl;
+			free(property);
 		}
 	}
-	free(cghtemp);
 #endif //DEBUG
 
 	//Determine number of cycles of TDMS file
