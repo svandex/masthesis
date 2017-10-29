0d5821a48b09a4d6da7d708e97bfffdb4740a457
svandex <svandex@users.noreply.github.com>
2017-10-29 19:25:15 +0800

    delete copy ctor
    delete copy ctor

--- code/ion.h
+++ code/ion.h
@@ -1,6 +1,5 @@
 #include<iostream>
 #include<nilibddc.h>
-#include<vector>
 
 //functor to display ddcError text
 class  ddcChk{
@@ -16,15 +15,21 @@ private:
 class ion{
 public:
 	ion(const char* p, const char* t);
-	ion(const ion&i)=delete;
+	ion(const ion& )=delete;
 	~ion();
+	void showProperty() const;
+	friend std::ostream& operator<< (std::ostream& os, const ion& i);//Output all properties of one channel
 
 protected:
 	DDCFileHandle file;
 	unsigned int numOfChannelGroups;
 	unsigned int numOfCycle;
 	template <unsigned M, unsigned N>
-		decltype(auto) getHandle(const unsigned int& M, const unsigned int& N);
+		decltype(auto) getHandle();//channel group handle or channel handle, for data aquisition
+
+	void split(unsigned int& numDataStart, unsigned int ) const;
+	//Determine the start number of data, used as an parameter to the function of data aquisition
+	//Use A-signal and Z-signal to determine, make sure the method has enough epsilong
 
 private:
 	const char* pepath;
@@ -40,8 +45,8 @@ struct ionAttr{
 //IO reading from TDMS file and writing cycle data to a file
 class ioncyc:public ion{
 public:
-	ioncyc(const ion& i,unsigned int n);
-	ioncyc(const ioncyc& i)=delete;
+	ioncyc(const ion& ionT,unsigned int n);
+	ioncyc(const ioncyc&)=delete;
 	void writeTo(const char* w);//save data of single cycle for plotting
 private:
 	unsigned int numCycle;
