1bbe1e8022ac070a5f519ab15aa16de97915bace
svandex <svandex@users.noreply.github.com>
2017-10-30 20:32:46 +0800

    template declaration and implementation should be in one file

--- code/ion.cpp
+++ code/ion.cpp
@@ -1,8 +1,12 @@
 #include<ion.h>
+#include<cstdlib>
+#include<typeinfo>
+#include<string>
 
 void ddcChk::operator()(const int &ddcError) const {
 	if(ddcError<0){
 		os<<"ddcError"<<sep<<ddcError<<":"<<DDC_GetLibraryErrorDescription(ddcError)<<std::endl;
+	//	throw;
 	}else{
 	//	std::cout<<"Check Pass!"<<std::endl;
 	}
@@ -13,67 +17,60 @@ ion::~ion(){
 	exChk(DDC_CloseFile(file));
 }
 
-ion::ion(const char* p, const char* t):pepath(p),tdmspath(t){
+ion::ion(const char* pe, const char* ionT):pepath(pe),ionTpath(ionT){
 	ddcChk errorChk(std::cerr);
 	//Open TDMS file
-	errorChk(DDC_OpenFileEx(tdmspath,DDC_FILE_TYPE_TDM_STREAMING,1,&file));
+	errorChk(DDC_OpenFileEx(ionTpath,DDC_FILE_TYPE_TDM_STREAMING,1,&file));
 	errorChk(DDC_GetNumChannelGroups(file,&numOfChannelGroups));
 
 	//Determine number of cycles of TDMS file
-	numOfCycle=0;//TODO LIST
+	numOfCycle=0;//TO DO LIST
 }
 
 //Show All channel properties
-void ion::showProperty() const{
+std::ostream& operator<< (std::ostream &os, const ion& ionT){
 	ddcChk errorChk;
-	DDCChannelGroupHandle* cghtemp=(DDCChannelGroupHandle*)calloc(numOfChannelGroups,sizeof(DDCChannelGroupHandle));
-	if(cghtemp == nullptr){std::cerr<<"DDCChannelGroupHandle allocation failed! "<<std::endl;return;}
-	for(size_t i=1;i<=numOfChannelGroups;i++){
-		std::cout<<"Channel Groups : "<<i<<std::endl;
-		unsigned int numOfChannels;
-		errorChk(DDC_GetNumChannels(cghtemp[i-1],&numOfChannels));
-		DDCChannelHandle* chtemp = (DDCChannelHandle* )calloc(numOfChannels,sizeof(DDCChannelHandle));
-	if(chtemp == nullptr){std::cerr<<"DDCChannelHandle allocation failed! "<<std::endl;return;}
-		errorChk(DDC_GetChannels(cghtemp[i-1],chtemp,numOfChannels));
+	DDCChannelGroupHandle* cghtemp=(DDCChannelGroupHandle*)std::calloc(ionT.numOfChannelGroups,sizeof(DDCChannelGroupHandle));
+	if(cghtemp == nullptr){std::cerr<<"DDCChannelGroupHandle allocation failed! "<<std::endl;return os;}
+	errorChk(DDC_GetChannelGroups(ionT.file,cghtemp,ionT.numOfChannelGroups));
 
-		for(size_t j=1;j<=numOfChannels;j++){
-			std::cout<<"	Channel : "<<j<<std::endl;
+	unsigned int numOfChannels;
+	//in each channel group
+	for(std::size_t i=1;i<=ionT.numOfChannelGroups;i++){
+		os<<"Channel Groups : "<<i<<std::endl;
+		errorChk(DDC_GetNumChannels(*(cghtemp+i-1),&numOfChannels));
+		DDCChannelHandle* chtemp = (DDCChannelHandle* )std::calloc(numOfChannels,sizeof(DDCChannelHandle));
+	if(chtemp == nullptr){std::cerr<<"DDCChannelHandle allocation failed! "<<std::endl;return os;}
+		errorChk(DDC_GetChannels(*(cghtemp+i-1),chtemp,numOfChannels));
+
+#ifdef DEBUG
+		std::cout<<"cghtemp is "<<cghtemp<<", typeinfo of cghtemp is "<<typeid(cghtemp).name()<<std::endl;
+		std::cout<<"typeinfo of *cghtemp is "<<typeid(*cghtemp).name()<<std::endl;
+		std::cout<<"DDCChannelGroupHandle typeinfo is "<<typeid(DDCChannelGroupHandle).name()<<std::endl;
+		std::cout<<"Line 40, numofCHannel is "<<numOfChannels<<std::endl;
+#endif //DEBUG
+
+		//each channel
+		for(std::size_t j=1;j<=numOfChannels;j++){
+			os<<"	Channel : "<<j<<std::endl;
 			unsigned int numOfProperties;
-			errorChk(DDC_GetNumChannelProperties(chtemp[j-1],&numOfProperties));
+			errorChk(DDC_GetNumChannelProperties(*(chtemp+j-1),&numOfProperties));
 			char* property=0;
 			unsigned int length;
+			std::string property_s;
 
-			errorChk(DDC_GetChannelStringPropertyLength(chtemp[j-1],DDC_CHANNEL_NAME,&length));
+			errorChk(DDC_GetChannelStringPropertyLength(*(chtemp+j-1),DDC_CHANNEL_NAME,&length));
 
 			property = (char*)malloc(length+1);
-			DDC_GetChannelProperty(chtemp[j-1],DDC_CHANNEL_NAME,property,length+1);
-			std::cout<<"		Property: "<<property<<std::endl;
+			errorChk(DDC_GetChannelProperty(chtemp[j-1],DDC_CHANNEL_NAME,property,length+1));
+			errorChk(DDC_GetChannelProperty(chtemp[j-1],DDC_CHANNEL_NAME,&property_s,0));
+
+			os<<"		Property: "<<property<<std::endl;
 			free(property);
 		}
+		std::free(chtemp);
 	}
+	std::free(cghtemp);
+	return os;
 }
 
-template<unsigned M, unsigned N>
-decltype(auto) ion::getHandle(){
-	if(M==0){
-		std::cerr<<"getHandle error: M cannot be 0.";
-		return nullptr;
-	}
-
-	ddcChk errorChk;
-	if(M>numOfChannelGroups){std::cerr<<"getHandle error: M exceeds channel groups number";return nullptr;}
-	DDCChannelGroupHandle* cghtemp=(DDCChannelGroupHandle*)calloc(numOfChannelGroups,sizeof(DDCChannelGroupHandle));
-	errorChk(DDC_GetChannelGroups(file,cghtemp,numOfChannelGroups));	
-	if(N==0){
-		return cghtemp[M];
-	}else{
-		unsigned int numOfChannels;
-		errorChk(DDC_GetNumChannels(cghtemp[M],&numOfChannels));	
-		if(N>numOfChannelGroups){std::cerr<<"getHandle error: N exceeds channels number";return nullptr;}
-		DDCChannelHandle* chtemp = (DDCChannelHandle* )calloc(numOfChannels,sizeof(DDCChannelHandle));
-		errorChk(DDC_GetChannels(cghtemp[M],chtemp,numOfChannels));
-		return chtemp[N];
-	}
-
-
-}
