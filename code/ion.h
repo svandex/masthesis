863a478d75f2895ba34c8bc99fc62048b5273b57
svandex <svandex@users.noreply.github.com>
2017-10-30 20:33:05 +0800

    template declaration and implementation should be in one file

--- code/ion.h
+++ code/ion.h
@@ -11,36 +11,59 @@ private:
 	char sep;
 };
 
-//Initial Processing of TDMS file and PE file, PE for photoelectric encoder.
+//Initial Processing of ion TDMS file and PE TDMS file, PE for photoelectric encoder.
 class ion{
 public:
-	ion(const char* p, const char* t);
+	ion(const char* pe, const char* ionT);
 	ion(const ion& )=delete;
 	~ion();
-	void showProperty() const;
 	friend std::ostream& operator<< (std::ostream& os, const ion& i);//Output all properties of one channel
+
+//protected:
+//
+//channel group handle or channel handle, for data aquisition
+	template<unsigned int M, unsigned int N>
+	decltype(auto) getHandle(){
+		if(M==0){
+			std::cerr<<"getHandle error: M cannot be 0.";
+			return nullptr;
+		}
+
+		ddcChk errorChk;
+		if(M>numOfChannelGroups){std::cerr<<"getHandle error: M exceeds channel groups number";return nullptr;}
+		DDCChannelGroupHandle* cghtemp=(DDCChannelGroupHandle*)std::calloc(numOfChannelGroups,sizeof(DDCChannelGroupHandle));
+		errorChk(DDC_GetChannelGroups(file,cghtemp,numOfChannelGroups));	
+		if(N==0){
+			return (cghtemp[M]);
+		}else{
+			unsigned int numOfChannels;
+			errorChk(DDC_GetNumChannels(cghtemp[M],&numOfChannels));	
+			if(N>numOfChannelGroups){std::cerr<<"getHandle error: N exceeds channels number";return nullptr;}
+			DDCChannelHandle* chtemp = (DDCChannelHandle* )std::calloc(numOfChannels,sizeof(DDCChannelHandle));
+			errorChk(DDC_GetChannels(cghtemp[M],chtemp,numOfChannels));
+			return (chtemp[N]);
+		}
+	}
+
+//specialization of getHandle<>()
+
+//ending of specialization
+
+	unsigned int split(std::pair<unsigned int, unsigned int> *p) const;
+	//Use A-signal and Z-signal to determine, make sure the method has enough epsilong
+	//use std::pair to hold each cycle's beginning number and ending number of ion-curve and cylinder-pressure-curve
+
+private:
+	const char* pepath;
+	const char* ionTpath;
 
 protected:
 	DDCFileHandle file;
 	unsigned int numOfChannelGroups;
 	unsigned int numOfCycle;
-	template <unsigned M, unsigned N>
-		decltype(auto) getHandle();//channel group handle or channel handle, for data aquisition
-
-	void split(unsigned int& numDataStart, unsigned int ) const;
-	//Determine the start number of data, used as an parameter to the function of data aquisition
-	//Use A-signal and Z-signal to determine, make sure the method has enough epsilong
-
-private:
-	const char* pepath;
-	const char* tdmspath;
 };
 
 std::ostream& operator<< (std::ostream& os, const ion& i);//Output all properties in format
-
-struct ionAttr{
-	
-};
 
 //IO reading from TDMS file and writing cycle data to a file
 class ioncyc:public ion{
